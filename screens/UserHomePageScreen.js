c2387e9f0c81347982ae9adda6e6ac1fabf50dd6
madat brahma <madatbrahma202@gmail.com>
2020-01-31 18:12:47 +0530

    adding userhomepage screen

--- screens/UserHomePageScreen.js
+++ screens/UserHomePageScreen.js
@@ -1,15 +1,87 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import EachDayWorkoutSummary from '../component/EachDayWorkoutSummary';
 import moment from "moment";
 
-class UserHomePageScreen extends Component {
+import db from '../common/db';
+import { UserDailyActivitySummary } from '../common/UseDailyActivitySummary';
+import DailyActivitySummary from '../component/DailyActivitySummary';
+
+class UserHomePageScreenV2 extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataLoaded: false,
+            dailyTrainingLogs: null,
+            weekTranings: []
+
+        };
+    }
+    populateActivitiSummary(snapshotVal) {
+        weekTraningsdata = [];
+
+        snapshotVal.map(function (each) {
+            let eachdayEntry = JSON.parse(JSON.stringify(each));
+            // console.log('each day entry is ',eachdayEntry);
+            //  let firstDateArr = Object.values(each);
+            let dateIs = eachdayEntry.date;
+            let dayIs = eachdayEntry.day;
+            let runnersArray = eachdayEntry.runners;
+            let runnersArrayIs = Object.values(runnersArray);
+            // console.log(dateIs, dayIs);
+            //  console.log(JSON.parse(JSON.stringify(runnersArray)) );
+
+            runnersArrayIs.map(function (runner) {
+                let runnerIs = runner.Name;
+                let woDesc = runner.desc;
+                if (runnerIs === 'Tilak') {
+                    let woSumary = new UserDailyActivitySummary(dateIs, dayIs, runnerIs, woDesc);
+                    //    console.log('adding new wo summary',woSumary);
+                    weekTraningsdata.push(woSumary);
+                }
+
+
+
+            });
+
+
+
+
+
+        })
+
+        console.log(weekTraningsdata);
+        this.setState({
+            weekTranings: weekTraningsdata,
+            dataLoaded: true
+        });
+
+
+    }
+
+    componentDidMount() {
+
+
+        var ref = db.ref("weekly-training/schedulesv2");
+
+
+
+        setTimeout(() => {
+            ref.orderByKey().startAt("02-01-2020").endAt("04-01-2020").on('value', (snapshot) => {
+
+                this.populateActivitiSummary(Object.values(snapshot.val()));
+
+            });
+
+        }, 3000);
+
+
+
+    }
 
     loadDetails() {
-      
-        var startOfWeek = moment().startOf('week').toDate();
-        var endOfWeek   = moment().endOf('week').toDate();
-        console.log(startOfWeek);
+
 
         this.props.navigation.navigate(
             'RunnerDailyActivityDetailsScreen', {
@@ -20,39 +92,71 @@ class UserHomePageScreen extends Component {
         }
         )
     }
+
     render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.userSummary}>
-                    <Text>User Summary</Text>
+
+        if (this.state.dataLoaded) {
+            return (
+
+                <View style={styles.container}>
+
+                    <FlatList
+                        data={this.state.weekTranings}
+                        renderItem={
+                            ({ item }) => <DailyActivitySummary
+                                name={item.name}
+                                desc={item.woSummary}
+                                loadDetails={() => this.loadDetails(item)} />
+                        }
+                    />
+
                 </View>
-                <View style={styles.weeklySection}>
+            );
+        } else {
+            return (
 
-                    <View style={styles.eachDay}>
-                        <EachDayWorkoutSummary day='Tuesday'
-                            date='04 Feb 2020'
-                            woSummary='400 repeats 20 times'
-                            loadDetails={() => this.loadDetails()} />
-                    </View>
-
-
-                    <View style={styles.eachDay}>
-                        <EachDayWorkoutSummary day='Thursday'
-                            date='06 Feb 2020'
-                            woSummary='8 Km Tempo' 
-                            loadDetails={() => this.loadDetails()} />
-                    </View>
-
-                    <View style={styles.eachDay}>
-                        <EachDayWorkoutSummary day='Saturday'
-                            date='08 Feb 2020'
-                            woSummary='25 Km Easy run' 
-                            loadDetails={() => this.loadDetails()} />
-                    </View>
+                <View>
+                    <Text>Data still loading</Text>
                 </View>
-            </View>
-        );
+            );
+        }
     }
+    /** render() {
+         if (this.state.dataLoaded) {
+            console.log('state has val ',this.state.dailyTrainingLogs);
+         }
+         return (
+             <View style={styles.container}>
+                 <View style={styles.userSummary}>
+                     <Text>User Summary</Text>
+                 </View>
+                 <View style={styles.weeklySection}>
+ 
+                     <View style={styles.eachDay}>
+                         <EachDayWorkoutSummary day='Tuesday'
+                             date='04 Feb 2020'
+                             woSummary='400 repeats 20 times'
+                             loadDetails={() => this.loadDetails()} />
+                     </View>
+ 
+ 
+                     <View style={styles.eachDay}>
+                         <EachDayWorkoutSummary day='Thursday'
+                             date='06 Feb 2020'
+                             woSummary='8 Km Tempo'
+                             loadDetails={() => this.loadDetails()} />
+                     </View>
+ 
+                     <View style={styles.eachDay}>
+                         <EachDayWorkoutSummary day='Saturday'
+                             date='08 Feb 2020'
+                             woSummary='25 Km Easy run'
+                             loadDetails={() => this.loadDetails()} />
+                     </View>
+                 </View>
+             </View>
+         );
+     }*/
 }
 
 const styles = StyleSheet.create({
@@ -80,4 +184,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default UserHomePageScreen;
+export default UserHomePageScreenV2;
